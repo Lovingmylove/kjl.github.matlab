7eead56a932690eb654d12cdd7ceba766ac91a26
Lovingmylove <kejinlong@bupt.edu.cn>
2016-04-11 16:38:33 +0800

    readtxt.m
    This program is writtrn to solve the problem of reading data in files
    with txt ending.

---
+++ readtxt.m
@@ -0,0 +1,15 @@
+txt = importdata('15.txt');
+plot(7:0.01:13.5,txt.data(700:1350))
+hold on
+a = 60.11;
+b = 59.83;
+plot(10.5:0.5:13.5,[a,a,a,a,a,a,a],'r-')
+hold on
+plot(10.5:0.5:13.5,[b,b,b,b,b,b,b],'r-')
+grid on
+axis([7,13.5,58,64])
+set(gca,'xtick',(7:0.5:13.5),'ytick',(58:0.5:64))
+xlabel('Ê±¼ä(min)')
+ylabel('ÎÂ¶È(T)')
+title(['deltaT=' num2str(a-b)])
+
